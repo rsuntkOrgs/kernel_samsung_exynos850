24556aff3fbca37452657835505b811204cc429c
Gabriel2392 <gabriel824m@gmail.com>
2025-04-25 09:21:32 -0300

    fingerprint: Force position 1

--- drivers/fingerprint/gw3x_common.c
+++ drivers/fingerprint/gw3x_common.c
@@ -707,9 +707,11 @@ int gw3x_get_gpio_dts_info(struct device *dev, struct gf_device *gf_dev)
 		gf_dev->orient = 0;
 	pr_info("orient: %d\n", gf_dev->orient);
 
-	if (of_property_read_string_index(np, "goodix,position", 0,
-			(const char **)&gf_dev->position))
-		gf_dev->position = "NA";
+	//if (of_property_read_string_index(np, "goodix,position", 0,
+			//(const char **)&gf_dev->position))
+		//gf_dev->position = "NA";
+
+	gf_dev->position = "1";
 	pr_info("position:%s\n", gf_dev->position);
 
 	if (of_property_read_u32(np, "goodix,spiclk_speed", &gf_dev->clk_setting->spi_speed))
