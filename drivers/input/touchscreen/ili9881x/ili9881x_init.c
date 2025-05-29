ae21c0df0c27464fae749507887b8caea8c8145d
rsuntk <rissu.ntk@gmail.com>
2025-05-29 16:35:45 +0000

    Revert "drivers: ilitek: add support for samsung dex touchpad"
    This reverts commit eb424a9801f7656b5756437f0d114f197bc133d1.
    
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

--- drivers/input/touchscreen/ili9881x/ili9881x_init.c
+++ drivers/input/touchscreen/ili9881x/ili9881x_init.c
@@ -21,7 +21,6 @@
  */
 
 #include "ili9881x.h"
-#include <linux/moduleparam.h>
 
 void ili_tp_reset(void)
 {
@@ -34,37 +33,6 @@ void ili_tp_reset(void)
 	usleep_range(5 * 1000, 5 * 1000);
 	gpio_set_value(ilits->tp_rst, 1);
 	msleep(ilits->rst_edge_delay);
-}
-
-bool __read_mostly ilits_support_dexpad = true;
-module_param(ilits_support_dexpad, bool, 0644);
-static void touch_set_input_prop_dexpad(struct input_dev *dev)
-{
-	static char ist_phys[64] = { 0 };
-	snprintf(ist_phys, sizeof(ist_phys), "%s/input1", dev->name);
-
-	dev->phys = ist_phys;
-	dev->id.bustype = BUS_I2C;
-	dev->dev.parent = ilits->dev;
-
-	set_bit(EV_SYN, dev->evbit);
-	set_bit(EV_KEY, dev->evbit);
-	set_bit(EV_ABS, dev->evbit);
-	set_bit(EV_SW, dev->evbit);
-	set_bit(BTN_TOUCH, dev->keybit);
-	set_bit(BTN_TOOL_FINGER, dev->keybit);
-	set_bit(KEY_BLACK_UI_GESTURE, dev->keybit);
-	set_bit(KEY_INT_CANCEL, dev->keybit);
-	set_bit(INPUT_PROP_POINTER, dev->propbit);
-	set_bit(KEY_HOMEPAGE, dev->keybit);
-
-	input_set_abs_params(dev, ABS_MT_POSITION_X, 0, ilits->max_x, 0, 0);
-	input_set_abs_params(dev, ABS_MT_POSITION_Y, 0, ilits->max_y, 0, 0);
-	input_set_abs_params(dev, ABS_MT_TOUCH_MAJOR, 0, 255, 0, 0);
-	input_set_abs_params(dev, ABS_MT_TOUCH_MINOR, 0, 255, 0, 0);
-	input_set_abs_params(dev, ABS_MT_CUSTOM, 0, 0xFFFFFFFF, 0, 0);
-
-	input_mt_init_slots(dev, 10, INPUT_MT_POINTER);
 }
 
 static void touch_set_input_prop_proximity(struct input_dev *dev)
@@ -105,21 +73,6 @@ void ili_input_register(void)
 
 		ilits->input_dev_proximity->name = "sec_touchproximity";
 		touch_set_input_prop_proximity(ilits->input_dev_proximity);
-	}
-
-	if (ilits_support_dexpad) {
-		ilits->input_dev_dexpad = input_allocate_device();
-		if (ilits->input_dev_dexpad == NULL) {
-			input_err(true, ilits->dev, "%s: allocate input_dev_dexpad err!\n", __func__);
-			if (ilits->input) {
-				input_free_device(ilits->input);
-				ilits->input = NULL;
-				return;
-			}
-		}
-
-		ilits->input_dev_dexpad->name = "sec_touchpad";
-		touch_set_input_prop_dexpad(ilits->input_dev_dexpad);
 	}
 
 	ilits->input->name = "sec_touchscreen";
@@ -180,22 +133,6 @@ void ili_input_register(void)
 			if (ilits->support_ear_detect) {
 				if (ilits->input_dev_proximity)
 					input_free_device(ilits->input_dev_proximity);
-			}
-			input_unregister_device(ilits->input);
-			ilits->input = NULL;
-		}
-	}
-
-	if (ilits_support_dexpad) {
-		ret = input_register_device(ilits->input_dev_dexpad);
-		if (ret < 0) {
-			input_err(true, ilits->dev, "%s: Unable to register %s input device\n",
-						__func__, ilits->input_dev_dexpad->name);
-
-			input_free_device(ilits->input);
-			if (ilits_support_dexpad) {
-				if (ilits->input_dev_dexpad)
-					input_free_device(ilits->input_dev_dexpad);
 			}
 			input_unregister_device(ilits->input);
 			ilits->input = NULL;
